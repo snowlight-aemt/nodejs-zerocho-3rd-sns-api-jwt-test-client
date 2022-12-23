6072e47778f9cecf9e223d2c23001333866ef506
snowlight-aemt <82430645+snowlight-aemt@users.noreply.github.com>
2022-12-23 20:59:04 +0900

    JWT 테스트 라우트

--- app.js
+++ app.js
@@ -39,3 +39,8 @@ app.use((err, req, res, next) => {
     res.status(err.status || 500);
     res.render('error');
 });
+
+
+app.listen(app.get('port'), () => {
+    console.log(app.get('port'), '번 포트에서 대기 중');
+})
---
+++ controllers/index.js
@@ -0,0 +1,28 @@
+const axios = require('axios');
+
+exports.test = async (req, res, next) => {
+    try {
+        if (!req.session.jwt) {
+            const tokenResult = await axios.post('http://localhost:8002/v1/token', {
+                clientSecret: process.env.CLIENT_SECRET,
+            });
+    
+            if (tokenResult.data?.code === 200) {
+                // 세션에 저장.
+                req.session.jwt = tokenResult.data.token;
+            } else {
+                return res.status(tokenResult.data?.code).json(tokenResult.data);
+            }
+        }
+        const result = await axios.get('http://localhost:8002/v1/test', {
+            headers: { authorization: req.session.jwt }
+        });
+        return res.json(result.data);
+    } catch(error) {
+        console.error(error);
+        if (error.response?.status === 419) {
+            return res.json(error.response.data);
+        } 
+        return next(error);
+    }
+}
