5d08e10fa9d6323a541bd9b8d21dc49487ecc394
snowlight-aemt <82430645+snowlight-aemt@users.noreply.github.com>
2022-12-26 23:42:22 +0900

    search posts

--- controllers/index.js
+++ controllers/index.js
@@ -1,28 +1,50 @@
 const axios = require('axios');
 
-exports.test = async (req, res, next) => {
+const URL = process.env.API_URL;
+axios.defaults.headers.origin = process.env.ORIGIN;
+
+const request = async (req, api) => {
     try {
         if (!req.session.jwt) {
-            const tokenResult = await axios.post('http://localhost:8002/v1/token', {
+            const tokenResult = await axios.post(`${URL}/token`, {
                 clientSecret: process.env.CLIENT_SECRET,
             });
     
-            if (tokenResult.data?.code === 200) {
-                // 세션에 저장.
-                req.session.jwt = tokenResult.data.token;
-            } else {
-                return res.status(tokenResult.data?.code).json(tokenResult.data);
-            }
+            req.session.jwt = tokenResult.data.token;
         }
-        const result = await axios.get('http://localhost:8002/v1/test', {
-            headers: { authorization: req.session.jwt }
+
+        return axios.get(`${URL}${api}`, {
+            headers: { authorization: req.session.jwt },
         });
-        return res.json(result.data);
-    } catch(error) {
-        console.error(error);
-        if (error.response?.status === 419) {
-            return res.json(error.response.data);
-        } 
-        return next(error);
+    } catch (err) {
+        // 처리할 수 있는 에러
+        console.log(err.response?.status);
+        if (err.response?.status === 419) {
+            delete req.session.jwt;
+            return request(req, api);
+        }
+        // 처리할 수 없는 에러...
+        // TODO 처음 보는 문법
+        throw err.response;
     }
-}
+};
+
+exports.getMyPosts = async (req, res, next) => {
+    try {
+        const result = await request(req, '/post/my');
+        res.json(result.data);
+    } catch (err) {
+        console.error(err);
+        next(err);
+    }
+};
+
+exports.searchByHashtag = async (req, res, next) => {
+    try {
+        const result = await request(req, `/post/hashtag/${encodeURIComponent(req.params.hashtag)}`);
+        res.json(result.data);
+    } catch (err) {
+        console.error(err);
+        next(err);
+    }
+};
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const { test } = require('../controllers');
+const { getMyPosts, searchByHashtag } = require('../controllers');
 const router = express.Router();
 
-router.get('/test', test);
+router.get('/myposts', getMyPosts);
+router.get('/search/:hashtag', searchByHashtag);
 
 module.exports = router;
